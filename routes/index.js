bef6815af040645361a2870e1a42c1a99de4458f
Nathan Eloe <powerofazure@gmail.com>
2021-10-27 13:55:53 -0500

    Demo mixins and includes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/neat.html', function(req, res, next) {
+  res.render('index', { title: 'CoolTown' });
+});
 module.exports = router;
